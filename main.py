6d4166375f34ea1c88931e7cb8540bc7557f8513
abaezaf <antonio.baezafdez@gmail.com>
2021-01-12 00:39:34 +0100

    Final Primera Clase Pygame

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame as pg
 import sys
 import random
+import entidades
 
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode((800, 600))
         pg.display.set_caption("Futuro Arkanoid")
+        self.pelota = entidades.Pelota(400, 300, 10, 10, (251, 202, 239), 25)
 
     def bucle_principal(self):
         game_over = False
@@ -16,8 +18,20 @@ class Game:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
+            if self.pelota.left <= 0 or self.pelota.right >= 800:
+                self.pelota.vx = -self.pelota.vx
+
+            if self.pelota.top <= 0 or self.pelota.bottom >= 600:
+                self.pelota.vy = -self.pelota.vy
+            
+            self.pelota.x += self.pelota.vx
+            self.pelota.y += self.pelota.vy
         
             self.pantalla.fill((0, 0, 255))
+            self.pantalla.blit(self.pelota.imagen, (self.pelota.x, self.pelota.y))
+
+
             pg.display.flip()
 
 
